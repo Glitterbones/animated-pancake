3ec46ffeac8d60c7be475b36c86c246a2d393eb7
ashley lockhart <allockhart49@gmail.com>
2022-02-19 16:30:56 -0700

    starting code

---
+++ script.js
@@ -0,0 +1,18 @@
+// Assignment code here
+var length;
+var number;
+var specialCharacter;
+var upperCase;
+var lowerCase;
+var selection;
+var lettersUp
+
+const characters = ["number", "letters", "character"];
+
+number = "0123456789";
+letters = "abcdefghijklmnopqrstuvwxyz";
+lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+character = "!@#$%^&*)()";
+
+
+generateBtn.addEventListener("click", writePassword);
