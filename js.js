154582b9542fb2939dceac52439d2715a9417d74
ashley lockhart <allockhart49@gmail.com>
2022-02-19 17:14:41 -0700

    var change

--- js.js
+++ js.js
@@ -1,11 +1,31 @@
-// Assignment code here
-var length;
-var number;
-var specialCharacter;
-var upperCase;
-var lowåerCase;
-var selection;
-var lettersUp
+var generateBtn = document.querySelector("#generate");
+var number = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","@","#","$","%","^","&","*","(",")","<",">","?","-","_","=","+","[","{","]","}"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const characters = ["number", "letters", "character"];
 
