6661cd00c49992561304ab145e49090df48de3e0
ashley lockhart <allockhart49@gmail.com>
2022-06-04 17:38:07 -0600

    working code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# animated-pancake
-password generator 
+## Password Generator
+A random password generator
+
+## Description
+This is an application that generates a random password based on criteria the user has selected. The user is provided with series of questions to determine password length and criteria which is then used to return a random password that meets those requirements.
+
+### Technologies Used
+* HTML
+* CSS
+* JavaScript
+
+### Screenshot
+![Screen Shot 2022-03-04 at 11 28 55 AM](https://user-images.githubusercontent.com/95373448/156821138-6125cabc-534e-4c0e-ba1a-1f1047e33dd0.png)
+![Screen Shot 2022-03-04 at 11 29 08 AM](https://user-images.githubusercontent.com/95373448/156821264-36b84cd0-2239-4dbc-9aff-c60feb3348ba.png)
+
+### Deployed Application
+https://jstable1.github.io/password-generator/
--- script.js
+++ script.js
@@ -1,18 +1,73 @@
 // Assignment code here
-var length;
-var number;
-var specialCharacter;
-var upperCase;
-var lowerCase;
-var selection;
-var lettersUp
+var specialCharactersArray = ["!", "?", "@", "$", "#", "%", "*", "<", ">", ".", "(", ")", "&", "+", "-", "/", "="];
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var userChoices = [];
 
-const characters = ["number", "letters", "character"];
+var passwordPrompts = function(){
+  var promptLength = window.prompt('The password must be between 8-128 characters. How long would you like the password to be?');
 
-number = "0123456789";
-letters = "abcdefghijklmnopqrstuvwxyz";
-lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-character = "!@#$%^&*)()";
+  if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return;
+  }
+
+  userChoices = [];
+
+  var specialChoice = window.confirm("Would you like to include special characters?");
+    if (specialChoice === true) {
+      userChoices = userChoices.concat(specialCharactersArray);
+    }
+
+  var numberChoice = window.confirm("Would you like to include numbers?");
+    if (numberChoice === true) {
+      userChoices = userChoices.concat(numberArray);
+    }
+
+  var lowerCaseChoice = window.confirm("Would you like to include lower case?");
+    if (lowerCaseChoice === true) {
+      userChoices = userChoices.concat(lowerCaseArray);
+    }
+
+  var upperCaseChoice = window.confirm("Would you like to include upper case?");
+    if (upperCaseChoice === true) {
+      userChoices = userChoices.concat(upperCaseArray);
+    }
+
+  if (specialChoice === false && numberChoice === false && lowerCaseChoice === false && upperCaseChoice === false) {
+    window.alert("You need to choose at least 1 character type");
+    return;
+  }
+
+  return promptLength
+}
+
+var generatePassword = function(){
+  var promptLength = passwordPrompts();
+
+  var passwordArray = []; 
+
+  for (var i = 0; i < promptLength; i++){
+    var randomNumber = Math.floor(Math.random()*userChoices.length)
+    passwordArray.push(userChoices[randomNumber])
+  };
+
+  return passwordArray.join("");
+}
 
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
